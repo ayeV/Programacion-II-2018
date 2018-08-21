fdf6a7aff101e01c8a3b33934960a2bea7868386
ayeV <36230640+ayeV@users.noreply.github.com>
2018-08-21 11:57:23 -0300

    Add files via upload

---
+++ Valdez.Ayelen.24/Ejercicio00/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Ingrese nombre: ");
+            nombre = Console.ReadLine();
+           // Console.WriteLine(nombre);
+            Console.WriteLine("Su nombre es: {0}", nombre);
+
+            
+              
+            Console.ReadLine();
+        }
+    }
+}
