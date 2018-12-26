330b6afa2cfe5b09216fe98b9cc3f69a7efa0809
ayeV <36230640+ayeV@users.noreply.github.com>
2018-12-26 14:16:28 -0300

    Add files via upload

---
+++ RepasoSql/Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace RepasoSql
+{
+    public class Product
+    {
+        public int productID;
+        public string productName;
+        public int supplierID;
+        public int categoryID;
+        public string quantityPerUnit;
+        public decimal unitPrice;
+        public int stock;
+        public int unitsInOrder;
+        public int reorderLevel;
+        public bool discontinued;
+
+
+        public Product(int prodID, string prodName, int suppID, int catID, string quantity, decimal price, int stock, int unitsOrder, int reorder, bool discontinued)
+        {
+            this.productID = prodID;
+            this.productName = prodName;
+            this.supplierID = suppID;
+            this.categoryID = catID;
+            this.quantityPerUnit = quantity;
+            this.unitPrice = price;
+            this.stock = stock;
+            this.unitsInOrder = unitsOrder;
+            this.discontinued = discontinued;
+        }
+
+
+        public bool AgregarProducto(Product p)
+        {
+            bool retorno = false;
+            SqlConnection connection = new SqlConnection(Properties.Settings.Default.Conexion);
+            SqlCommand command = new SqlCommand();
+            
+            command.CommandText = string.Format("INSERT INTO Products values ({0}, '{1}', {2}, {3}, '{4}', {5}, {6}, {7}, {8}, {9})", p.productID, p.productName, p.supplierID, p.categoryID, p.quantityPerUnit, p.unitPrice, p.stock, p.unitsInOrder, p.reorderLevel, p.discontinued);
+            command.CommandText = "SET IDENTITY_INSERT Products  ON";
+            command.CommandType = System.Data.CommandType.Text;
+            command.Connection = connection;
+
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+                retorno = true;
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                retorno = false;
+            }
+
+            return retorno;
+        }
+
+    }
+}
---
+++ RepasoSql/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace RepasoSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Listado Completo
+            //Console.Write(Program.ObtenerListado());
+
+            //Listado ordenado por nombre, solo muestra nombres
+            //Console.Write(Program.Mostrar1());
+
+          //  Console.WriteLine(Program.Mostrar2());
+
+            Product p1 = new Product(78, "Durazno", 15, 1, "52 cajas", (decimal)14.00, 30, 78, 8, false);
+           if( p1.AgregarProducto(p1))
+            {
+                Console.Write("Producto agregado con exito");
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static string ObtenerListado()
+        {
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.Conexion);
+            SqlCommand comando = new SqlCommand();
+            comando.CommandText = "SELECT * FROM dbo.Products";
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.Connection = conexion;
+
+            conexion.Open();
+
+            SqlDataReader lector = comando.ExecuteReader();
+            var sb = new StringBuilder();
+            sb.AppendLine("ProductID   ProductName    SupplierID    Quantity    UnitPrice    Stock    UnitsInOrder   ReorderLevel  Discontinued\n");
+            while (lector.Read())
+            {
+                sb.AppendLine(string.Format("{0} || {1} || {2} || {3} || {4} || {5} || {6} || {7}\n", lector[0], lector[1], lector[2], lector[3], lector[4], lector[5], lector[6], lector[7]));
+            }
+            conexion.Close();
+
+            return sb.ToString();
+
+
+
+
+        }
+
+        private static string Mostrar1()
+        {
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.Conexion);
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "SELECT ProductName FROM dbo.Products order by ProductName";
+            command.CommandType = System.Data.CommandType.Text;
+            command.Connection = conexion;
+
+            conexion.Open();
+
+            SqlDataReader lector = command.ExecuteReader();
+            var sb = new StringBuilder();
+            sb.AppendLine("ProductName\n");
+            while (lector.Read())
+            {
+                sb.AppendLine(string.Format("{0}", lector[0]));
+            }
+
+
+           
+                conexion.Close();
+
+
+               
+           
+
+            return sb.ToString();
+
+        }
+
+        private static string Mostrar2()
+        {
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.Conexion);
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "SELECT pro.ProductName, cat.CategoryName FROM Products pro INNER JOIN Categories cat ON (pro.CategoryID = cat.CategoryID)";
+            command.CommandType = System.Data.CommandType.Text;
+            command.Connection = conexion;
+
+            conexion.Open();
+
+            SqlDataReader lector = command.ExecuteReader();
+            var sb = new StringBuilder();
+         
+            while (lector.Read())
+            {
+                sb.AppendLine(string.Format("Product Name: {0}\nCategoryName: {1}\n", lector[0],lector[1]));
+            }
+
+
+
+            conexion.Close();
+
+
+
+
+
+            return sb.ToString();
+        }
+        
+    }
+}
